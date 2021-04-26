62f66cefe3911021e3893e9853c4fb9f5da7d615
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2021-04-25 19:33:11 -0500

    End of section 19

--- client-app/src/App/api/agent.ts
+++ client-app/src/App/api/agent.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
+import { profile } from 'node:console';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity, ActivityFormValues } from '../models/Activity';
@@ -91,7 +92,8 @@ const Profiles = {
         })
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+    updateProfile: (profile: Partial<Profile>) => requests.put('/profiles', profile)
 }
 
 const agent = {
